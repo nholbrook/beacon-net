05683b55daa7d4c221f27da39549faf4ec954189
Nick Holbrook <ndh175@gmail.com>
2019-01-28 21:24:01 -0500

    Initial commit

---
+++ src/test/java/com/nickholbrook/beaconnet/BeaconNetApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nickholbrook.beaconnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeaconNetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
