d47accd43d1b734d8c4fde41db55e1c47d15a30e
Nick Holbrook <ndh175@gmail.com>
2019-02-12 15:40:06 -0500

    Integrated CoreUI

--- src/main/java/com/nickholbrook/beaconnet/controllers/HomeController.java
+++ src/main/java/com/nickholbrook/beaconnet/controllers/HomeController.java
@@ -5,22 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
 
 	// If not authenticated
 
+	/*
 	@RequestMapping(method = RequestMethod.GET)
 	public String getMethod() {
 		return "redirect:login";
 	}
+	*/
 
 	//If authenticated
-
-	/*
-		@RequestMapping(method = RequestMethod.GET)
-		public String getMethod() {
-			return "home";
-		}
-	*/
+	@RequestMapping(method = RequestMethod.GET)
+	public String getHome() {
+		return "index";
+	}
 }
