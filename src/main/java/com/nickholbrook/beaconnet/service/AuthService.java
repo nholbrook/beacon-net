6295511972540753499fc65c6c7ce84d4f273881
sbouch <46934530+sbouch@users.noreply.github.com>
2019-04-07 11:26:23 -0400

    Update AuthService.java

--- src/main/java/com/nickholbrook/beaconnet/service/AuthService.java
+++ src/main/java/com/nickholbrook/beaconnet/service/AuthService.java
@@ -24,66 +24,67 @@ public class AuthService {
 			region = us-east-1
 	identityPoolId = us-east-1:f2810be3-a906-4a1e-83bc-aa1230b6789*/
 
-	final static Regions REGION;
-	final static String ENDPOINT;
-	final static String USER_POOL_ID;
-	final static String CLIENT_ID;
-	final static String IDENTITY_POOL_ID;
+    final static Regions REGION;
+    final static String ENDPOINT;
+    final static String USER_POOL_ID;
+    final static String CLIENT_ID;
+    final static String IDENTITY_POOL_ID;
 
-	static {
-		Map<String, String> config = LoadFromProperties.loadFromProperties();
+    static {
+        Map<String, String> config = LoadFromProperties.loadFromProperties();
 
-		ENDPOINT = LoadFromProperties.fetchValue(config, "aws.cognito.endpoint");
-		USER_POOL_ID = LoadFromProperties.fetchValue(config, "aws.cognito.userPoolId");
-		CLIENT_ID = LoadFromProperties.fetchValue(config, "aws.cognito.clientId");
-		IDENTITY_POOL_ID = LoadFromProperties.fetchValue(config, "aws.cognito.identityPoolId");
-		REGION = Regions.valueOf(LoadFromProperties.fetchValue(config, "aws.region"));
+        ENDPOINT = LoadFromProperties.fetchValue(config, "cognito-idp.us-east-1.amazonaws.com");
+        USER_POOL_ID = LoadFromProperties.fetchValue(config, "us-east-1_3vocxnITQ");
+        CLIENT_ID = LoadFromProperties.fetchValue(config, "2jkihs1a8su8n4jq0lvihsh3po");
+        IDENTITY_POOL_ID = LoadFromProperties.fetchValue(config, "us-east-1:f2810be3-a906-4a1e-83bc-aa1230b6789");
+        REGION = Regions.valueOf(LoadFromProperties.fetchValue(config, "us-east-1"));
 
-	}
+    }
 
-	AuthService() {
-		System.out.println(getUserInfo("sboucher").toString());
-	}
+    AuthService() {
+        System.out.println(getUserInfo("sboucher").toString());
+    }
 
-	public AWSCognitoIdentityProvider getAmazonCognitoIdentityClient() {
-		ClasspathPropertiesFileCredentialsProvider propertiesFileCredentialsProvider =
-				new ClasspathPropertiesFileCredentialsProvider();
+    public AWSCognitoIdentityProvider getAmazonCognitoIdentityClient() {
+        ClasspathPropertiesFileCredentialsProvider propertiesFileCredentialsProvider =
+                new ClasspathPropertiesFileCredentialsProvider();
 
-		return AWSCognitoIdentityProviderClientBuilder.standard()
-				.withCredentials(propertiesFileCredentialsProvider)
-				.withRegion( REGION )
-				.build();
+        return AWSCognitoIdentityProviderClientBuilder.standard()
+                .withCredentials(propertiesFileCredentialsProvider)
+                .withRegion(REGION)
+                .build();
 
-	}
+    }
 
-	public UserResponse getUserInfo(String username) {
+    public UserResponse getUserInfo(String username) {
 
-		AWSCognitoIdentityProvider cognitoClient = getAmazonCognitoIdentityClient();
-		AdminGetUserRequest userRequest = new AdminGetUserRequest()
-				.withUsername(username)
-				.withUserPoolId( USER_POOL_ID );
+        AWSCognitoIdentityProvider cognitoClient = getAmazonCognitoIdentityClient();
+        AdminGetUserRequest userRequest = new AdminGetUserRequest()
+                .withUsername(username)
+                .withUserPoolId(USER_POOL_ID);
 
 
-		AdminGetUserResult userResult = cognitoClient.adminGetUser(userRequest);
+        AdminGetUserResult userResult = cognitoClient.adminGetUser(userRequest);
 
-		UserResponse userResponse = new UserResponse();
-		userResponse.setUsername(userResult.getUsername());
-		userResponse.setUserStatus(userResult.getUserStatus());
-		userResponse.setUserCreateDate(userResult.getUserCreateDate());
-		userResponse.setLastModifiedDate(userResult.getUserLastModifiedDate());
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUsername(userResult.getUsername());
+        userResponse.setUserStatus(userResult.getUserStatus());
+        userResponse.setUserCreateDate(userResult.getUserCreateDate());
+        userResponse.setLastModifiedDate(userResult.getUserLastModifiedDate());
 
-		List<AttributeType> userAttributes = userResult.getUserAttributes();
-		for(AttributeType attribute: userAttributes) {
-			if(attribute.getName().equals("custom:companyName")) {
-				userResponse.setCompanyName(attribute.getValue());
-			}else if(attribute.getName().equals("custom:companyPosition")) {
-				userResponse.setCompanyPosition(attribute.getValue());
-			}else if(attribute.getName().equals("email")) {
-				userResponse.setEmail(attribute.getValue());
-			}
-		}
+        List<AttributeType> userAttributes = userResult.getUserAttributes();
+        for (AttributeType attribute : userAttributes) {
+            if (attribute.getName().equals("custom:companyName")) {
+                userResponse.setCompanyName(attribute.getValue());
+            } else if (attribute.getName().equals("custom:companyPosition")) {
+                userResponse.setCompanyPosition(attribute.getValue());
+            } else if (attribute.getName().equals("email")) {
+                userResponse.setEmail(attribute.getValue());
+            }
+        }
 
-		cognitoClient.shutdown();
-		return userResponse;
-	}
+        cognitoClient.shutdown();
+        return userResponse;
+    }
+
 }
